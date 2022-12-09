54bf347a1b1e2f05f4355e68c0eedf512348730d
Pico2x <pico2x@gmail.com>
2022-12-09 01:30:57 -0300

    First rudimentary ChatGPT PR Review
    - Uses revChatGPT as an API for ChatGPT.
    - Passes chatGPT the code of the files modified in the PR.
    - Asks it to review the code.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+from ast import List
 import base64
 import json
 import requests
 from patch import fromstring
 import urllib
+from revChatGPT.revChatGPT import Chatbot
+
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+config = {
+   
+    "session_token": "<SESSION_TOKEN>"
+}
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -40,6 +47,11 @@ class RequestHandler(BaseHTTPRequestHandler):
         # Create the modified files array
         modified_files = self.create_modified_files_array(pull_request)
 
+        # Review the pull request
+        review = self.review_pull_request(modified_files)
+        print("Review: ")
+        print(review)
+
     def create_modified_files_array(self, pull_request):
         # Initialize the modified files array
         modified_files = []
@@ -66,7 +78,7 @@ class RequestHandler(BaseHTTPRequestHandler):
            
             # Use the GitHub API to get the contents of the modified file
             url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
-            headers = {'Authorization': 'token <YOUR_AUTH_TOKEN>'}
+            headers = {'Authorization': 'token <AUTH_TOKEN>'}
             try:
                 response = requests.get(url, headers=headers)
                 data = response.json()
@@ -81,6 +93,22 @@ class RequestHandler(BaseHTTPRequestHandler):
             modified_files.append(code)
 
         return modified_files
+
+    def review_pull_request(self, modified_files):
+      # Create a Chatbot instance
+      chatbot = Chatbot(config)
+
+      chatbot.get_chat_response("I will send you a series files corresponding to a Github Pull Request. Please simply write ACKNOWLEDGE after each one.", output="text")
+
+      # Send the modified files to the chatbot
+      for file in modified_files:
+          chatbot.get_chat_response(file, output="text")
+
+      # Ask the chatbot to review the pull request
+      review = chatbot.get_chat_response("We would like you imagine the application has been written by a junior colleague and code-review it in GitHub. We are particularly interested in seeing you: Find code smells or questionable technical decisions, Identify bugs, Explain issues in writing to the code author", output="text")
+
+      # Return the chatbot's review
+      return review['message']
 
 
 httpd = HTTPServer(('localhost', 8000), RequestHandler)
