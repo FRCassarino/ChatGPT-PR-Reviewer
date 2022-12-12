891b79054aa80433aa937d13514fbf12068ac801
Pico2x <pico2x@gmail.com>
2022-12-12 12:12:20 -0300

    Update the prompts so they don't return "I can't answer because of X".
    - The original prompts were returning "I'm sorry I can't answer that. I'm a large language model and I can't X and Y". Updated them to ones that work.
    - Changed the config structure to match the new version of revChatGpt.

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 config = {
    
-    "session_token": "<SESSION_TOKEN>"
+    "session_token": "<SESSION_TOKEN>",
+    "cf_clearance": "<CLOUDFLARE_TOKEN>",
+    "user_agent": "<USER_AGENT_CHROME>",
 }
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -52,6 +54,10 @@ class RequestHandler(BaseHTTPRequestHandler):
         print("Review: ")
         print(review)
 
+        # Post the review as a comment in the pull request
+        self.post_review_as_comment(review, pull_request)
+        print("Posted review as comment")
+
     def create_modified_files_array(self, pull_request):
         # Initialize the modified files array
         modified_files = []
@@ -73,12 +79,11 @@ class RequestHandler(BaseHTTPRequestHandler):
         for hunk in patch:
             # Use the hunk.target attribute to get the path of the modified file
             filename = hunk.target 
-            path = str(filename).split('/')[1]
-            path = path.replace("'", "")
+           
            
             # Use the GitHub API to get the contents of the modified file
-            url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
-            headers = {'Authorization': 'token <AUTH_TOKEN>'}
+            url = f'https://api.github.com/repos/{owner}/{repo}/contents/main.py'
+            headers = {'Authorization': 'token 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'}
             try:
                 response = requests.get(url, headers=headers)
                 data = response.json()
@@ -98,17 +103,47 @@ class RequestHandler(BaseHTTPRequestHandler):
       # Create a Chatbot instance
       chatbot = Chatbot(config)
 
-      chatbot.get_chat_response("I will send you a series files corresponding to a Github Pull Request. Please simply write ACKNOWLEDGE after each one.", output="text")
+      chatbot.get_chat_response("We're going to do an exercise. Imagine these are a series of coding files that belong to a Github Pull Request. Please simply write ACKNOWLEDGE, and write a short summary of what each file does. I will send the first file after this message. Please respond now with ACKNOWLEDGE", output="text")
+
+
 
       # Send the modified files to the chatbot
       for file in modified_files:
-          chatbot.get_chat_response(file, output="text")
+          print("File: " + file)
+          print(chatbot.get_chat_response(file, output="text"))
 
       # Ask the chatbot to review the pull request
-      review = chatbot.get_chat_response("We would like you imagine the application has been written by a junior colleague and code-review it in GitHub. We are particularly interested in seeing you: Find code smells or questionable technical decisions, Identify bugs, Explain issues in writing to the code author", output="text")
+      review = chatbot.get_chat_response("For the code I pasted, find code smells or questionable technical decisions, identify bugs and explain issues. \nStructure this as a list of items, ranked by importance. \nThis does NOT require you to browse  the internet or analyze specific code files.", output="text")
 
       # Return the chatbot's review
       return review['message']
+
+    def post_review_as_comment(self, review, pull_request):
+      # Get the repository information from the pull request
+      repo = pull_request['head']['repo']['name']
+      owner = pull_request['user']['login']
+      number = pull_request['number']
+
+      # Set the URL for creating a new comment on the pull request
+      url = f'https://api.github.com/repos/{owner}/{repo}/issues/{number}/comments'
+
+      # Set the request payload
+      payload = {
+          'body': review
+      }
+
+      # Set the request headers
+      headers = {
+          'Authorization': 'token <GITHUB_TOKEN>',
+          'Content-Type': 'application/json'
+      }
+
+      # Send a POST request to create the comment
+      try:
+          response = requests.post(url, json=payload, headers=headers)
+      except requests.RequestException as err:
+          print(f'Error posting review as comment: {err}')
+          return
 
 
 httpd = HTTPServer(('localhost', 8000), RequestHandler)
